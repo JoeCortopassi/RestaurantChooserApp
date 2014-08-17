a92c4ea0d43b7ca666462581e588fe4390811e70
Joe Cortopassi <joe@joecortopassi.com>
2014-08-16 18:14:17 -0700

    Initial commit. Empty project

---
+++ RestaurantChooserApp/RCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCAppDelegate.h
+//  RestaurantChooserApp
+//
+//  Created by Joseph Cortopassi on 8/16/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RestaurantChooserApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RestaurantChooserApp
+//
+//  Created by Joseph Cortopassi on 8/16/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCAppDelegate class]));
+    }
+}
